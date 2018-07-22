f2dce5ef775b2bed58e8d43cdf495d929ffb9430
Clark Li <clark.li86@gmail.com>
2018-07-22 20:37:49 +0930

    Drives okay on straight and smooth curves
    In sharp corners it is severely understeering. Need more data at sharp
    corners

--- train.py
+++ train.py
@@ -7,12 +7,12 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from keras.models import Sequential, Model
 from keras.layers import Flatten, Dense, Lambda, Input, merge
-from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import Convolution2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
-from keras.engine.topology import Merge
 from keras.layers.core import Dropout
 from keras.callbacks import *
 from keras.optimizers import Adam
+from keras import backend as K
 
 TRAINING_DATA_DIR = 'training_data/basic_lap/'
 
@@ -44,16 +44,25 @@ for image, measurement in zip(images, measurements):
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
-def output_layer(x):
-    return x
-
-def output_shape(x):
-    return x
+def display_cropped(model, image):
+    """
+    Output from cropping layer. Will be useful for writeup
+    """
+    cropping_layer = model.get_layer('cropping2d_1')
+    cropping_function = K.function([cropping_layer.input], [cropping_layer.output])
+    cropped_image = cropping_function([image.reshape(1, 160, 320, 3)])[0]
+    plt.title('Original')
+    plt.imshow(image)
+    plt.show()
+    plt.title('Cropped')
+    plt.imshow(np.uint8(cropped_image.reshape(70, 320, 3)))
+    plt.show()
 
 def Traffic_Net():
     net_input = Input(shape=(160, 320, 3))
     pool1 = net_input
     pool1 = Lambda(lambda x: (x / 255.0) - 0.5)(pool1)
+    pool1 = Cropping2D(cropping=((65,25), (0,0)), input_shape=(3,160,320))(pool1)
     pool1 = Convolution2D(nb_filter=12, nb_row=3, nb_col=3, activation='relu', border_mode='valid')(pool1)
     pool1 = Convolution2D(nb_filter=24, nb_row=3, nb_col=3, activation='relu', border_mode='valid')(pool1)
     pool1 = MaxPooling2D(pool_size=(2, 2), border_mode='valid')(pool1)
@@ -78,9 +87,10 @@ def Traffic_Net():
 model = Traffic_Net()
 model.summary()
 # TODO Exponential delay learning rate
-optimizer = Adam(lr=0.0005)
+optimizer = Adam(lr=0.001)
 model.compile(loss='mse', optimizer=optimizer)
 save_checkpointer = ModelCheckpoint(filepath="model.h5", monitor='val_loss', verbose=1, save_best_only=True)
-stop_checkpointer = EarlyStopping(monitor='val_loss', min_delta=0, patience=3, verbose=1, mode='auto')
+stop_checkpointer = EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=1, mode='auto')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10, callbacks=[save_checkpointer, stop_checkpointer])
+#display_cropped(model, X_train[0])
 exit()
