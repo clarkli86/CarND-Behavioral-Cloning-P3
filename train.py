5cee18c43140d7d3755725c9fe394cf3cc5209b3
Clark Li <clark.li86@gmail.com>
2018-07-23 21:56:24 +0930

    First version of nvidia net
    Able to drive through first track but terrible on the second one

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Lambda, Input, merge
+from keras.layers import Flatten, Dense, Lambda, Input, merge, ELU
 from keras.layers.convolutional import Convolution2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.core import Dropout
@@ -37,6 +37,7 @@ for TRAINING_DATA_DIR in TRAINING_DATA_DIRS:
         filename = source_path.split('/')[-1]
         current_path = TRAINING_DATA_DIR  + 'IMG/' + filename
         image = cv2.imread(current_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         images.append(image)
         # Steering angle
         measurement = float(line[3])
@@ -70,7 +71,7 @@ def traffic_net():
     net_input = Input(shape=(160, 320, 3))
     pool1 = net_input
     pool1 = Lambda(lambda x: (x / 255.0) - 0.5)(pool1)
-    pool1 = Cropping2D(cropping=((65,25), (0,0)), input_shape=(3,160,320))(pool1)
+    pool1 = Cropping2D(cropping=((65,25), (0,0)), input_shape=(160,320, 3))(pool1)
     pool1 = Convolution2D(nb_filter=12, nb_row=3, nb_col=3, activation='relu', border_mode='valid')(pool1)
     pool1 = Convolution2D(nb_filter=24, nb_row=3, nb_col=3, activation='relu', border_mode='valid')(pool1)
     pool1 = MaxPooling2D(pool_size=(2, 2), border_mode='valid')(pool1)
@@ -92,34 +93,57 @@ def traffic_net():
     model = Model(input=net_input, output=fc)
     return model
 
-def nvdia_net():
-    net_input = Input(shape=(160, 320, 3))
-    pool1 = net_input
-    pool1 = Lambda(lambda x: (x / 255.0) - 0.5)(pool1)
-    pool1 = Cropping2D(cropping=((65,25), (0,0)), input_shape=(3,160,320))(pool1)
-    pool1 = Convolution2D(nb_filter=24, nb_row=5, nb_col=5, sub_sample=(2, 2), activation='relu', border_mode='valid')(pool1)
-    pool1 = Convolution2D(nb_filter=36, nb_row=5, nb_col=5, sub_sample=(2, 2), activation='relu', border_mode='valid')(pool1)
-    pool1 = Convolution2D(nb_filter=48, nb_row=5, nb_col=5, sub_sample=(2, 2), activation='relu', border_mode='valid')(pool1)
-    pool1 = Convolution2D(nb_filter=64, nb_row=3, nb_col=3, sub_sample=(1, 1), activation='relu', border_mode='valid')(pool1)
-    pool1 = Convolution2D(nb_filter=64, nb_row=3, nb_col=3, sub_sample=(1, 1), activation='relu', border_mode='valid')(pool1)
+def nvidia_net():
+    model = Sequential()
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320, 3), output_shape=(160,320, 3)))
+    model.add(Cropping2D(cropping=((65,25), (0,0)), input_shape=(160,320, 3)))
+    model.add(Convolution2D(nb_filter=24, nb_row=5, nb_col=5, subsample=(2, 2), border_mode='valid'))
+    model.add(ELU())
+    model.add(Convolution2D(nb_filter=36, nb_row=5, nb_col=5, subsample=(2, 2), border_mode='valid'))
+    model.add(ELU())
+    model.add(Convolution2D(nb_filter=48, nb_row=5, nb_col=5, subsample=(2, 2), border_mode='valid'))
+    model.add(ELU())
+    model.add(Convolution2D(nb_filter=64, nb_row=3, nb_col=3, subsample=(1, 1), border_mode='valid'))
+    model.add(ELU())
+    model.add(Convolution2D(nb_filter=64, nb_row=3, nb_col=3, subsample=(1, 1), border_mode='valid'))
+    model.add(ELU())
 
-    pool1 = Flatten()(pool1)
+    model.add(Flatten())
 
-    fc = Dense(100, activation='relu')(pool1)
-    fc = Dense(50, activation='relu')(pool1)
-    fc = Dense(10, activation='relu')(pool1)
-    fc = Dense(1)(fc)
+    model.add(Dense(100))
+    model.add(ELU())
+    model.add(Dense(50))
+    model.add(ELU())
+    model.add(Dense(10))
+    model.add(ELU())
+    model.add(Dense(1))
 
-    model = Model(input=net_input, output=fc)
     return model
 
-model = traffic_net()
+def common_ai_net():
+    ch, row, col = 3, 160, 320  # camera format
+    model = Sequential()
+    model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(ch, row, col), output_shape=(ch, row, col)))
+    model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same"))
+    model.add(ELU())
+    model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode="same"))
+    model.add(ELU())
+    model.add(Convolution2D(64, 5, 5, subsample=(2, 2), border_mode="same"))
+    model.add(Flatten())
+    model.add(Dropout(.2))
+    model.add(ELU())
+    model.add(Dense(512))
+    model.add(Dropout(.5))
+    model.add(ELU())
+    model.add(Dense(1))
+    return model
+
+model = nvidia_net()
 model.summary()
 # TODO Exponential delay learning rate
-optimizer = Adam(lr=0.001)
-model.compile(loss='mse', optimizer=optimizer)
-save_checkpointer = ModelCheckpoint(filepath="model.h5", monitor='val_loss', verbose=1, save_best_only=True)
-stop_checkpointer = EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=1, mode='auto')
+model.compile(loss='mse', optimizer='adam')
+save_checkpointer = ModelCheckpoint(filepath="nvidia.model.h5", monitor='val_loss', verbose=1, save_best_only=True)
+stop_checkpointer = EarlyStopping(monitor='val_loss', min_delta=0, patience=3, verbose=1, mode='auto')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10, callbacks=[save_checkpointer, stop_checkpointer])
 #display_cropped(model, X_train[0])
 exit()
